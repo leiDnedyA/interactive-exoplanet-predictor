c2795856aafc488e44d25cbbdc15359472551896
Ayden <aydendiel@gmail.com>
2022-11-07 20:10:45 -0500

    Created and styled predictions page

--- client/src/pages/Predictions.js
+++ client/src/pages/Predictions.js
@@ -1,6 +1,33 @@
+import { useLocation } from 'react-router-dom';
+import './Predictions.css';
+
+/**
+ * Shape of "data" location state
+ * 
+ * [{
+ *  property: string (e.g Number of planets, density, etc),
+ *  value: any (e.g 7.5, 11kg, Andromeda)
+ * }]
+ * 
+ */
 
 const Predictions = () => {
-    return <div></div>
+
+    const location = useLocation();
+    const data = location.state.data;
+
+    const dataElements = [];
+
+    for (let i in data) {
+        let d = data[i];
+        let e = <p className="predicted-value" key={i}><strong>{d.property}</strong>: {d.value}</p>
+        dataElements.push(e);
+    }
+
+    return <div className="predictions">
+        <h3>Data about star</h3>
+        {dataElements}
+    </div>
 }
 
 export default Predictions;
