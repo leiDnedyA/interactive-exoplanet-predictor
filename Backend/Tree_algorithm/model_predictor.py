3744c1de171de0eebe99297acf97eef283d6cec3
Edward Gaibor <78832141+gaiborjosue@users.noreply.github.com>
2022-11-09 18:35:04 -0500

    min_max_train

--- Backend/Tree_algorithm/model_predictor.py
+++ Backend/Tree_algorithm/model_predictor.py
@@ -1,9 +1,6 @@
 import pandas as pd
 import joblib
 
-# Args needed for the function, the values for x as a list:
-simple_args = ['stellareffectivetemp', 'stellarradius', 'stellarmass', 'stellarmetallicity', 'stellarage', 'stellardensity', 'stellarradialvelocity', 'stellsurfacegravity']
-
 # Load the model
 planet_predictor = joblib.load('Backend\Tree_algorithm\Saved_models\exoplanet_predictor.joblib')
 
