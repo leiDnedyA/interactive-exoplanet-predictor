8944d4094ad5c7da1427b6ec3a1c989388e5bb61
Ayden <aydendiel@gmail.com>
2022-11-09 09:58:30 -0500

    Cleaned up TextForm text formatting

--- client/src/elements/TextForm.js
+++ client/src/elements/TextForm.js
@@ -1,13 +1,20 @@
 
-//textFields should be a list of strings
+//textFields should be a list of strings, format: ['word_word', 'second_element', ...]
 const TextForm = ({ textFields, className, onSubmit }) => {
     
     const fieldElements = []
 
     for(let i in textFields){
+        //styles string in different ways
         let field = textFields[i];
-        let label = <label key={`label${i}`} for={field}>{field}:</label>
-        let input = <input key={`input${i}`} className="input text-input" name={field} type="text" placeholder={`${field} of star...`} />
+        let text = field.replace('_', ' ');
+        let capitalizedText = text.split(' ').map(v=>v[0].toUpperCase() + v.substring(1)).join(' ');
+
+        //creates elements based on string
+        let label = <label key={`label${i}`} for={field}>{capitalizedText}:</label>
+        let input = <input key={`input${i}`} className="input text-input" name={field} type="text" placeholder={`${text} of star...`} />
+        
+        //adds elements to fieldElements list
         fieldElements.push(label);
         fieldElements.push(input);
     }
