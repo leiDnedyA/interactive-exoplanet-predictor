baa20a6ce30057455bb0b236df131deffabfe9fc
Ayden <aydendiel@gmail.com>
2022-11-13 11:12:00 -0500

    Added game loop to WorldEngine.js

--- client/src/world_model/WorldEngine.js
+++ client/src/world_model/WorldEngine.js
@@ -8,12 +8,20 @@ class WorldEngine {
         this.scene = new THREE.Scene();
         this.camera = new THREE.PerspectiveCamera(75, 1, 0.1, 1000);
         this.renderer = new THREE.WebGLRenderer({ canvas: this.canvas });
+        this.lastTime = Date.now();
     }
 
     update() {
+
+        //setting deltaTime
+        let now = Date.now();
+        let deltaTime = now - this.lastTime;
+        this.lastTime = now;
+
+        
+
         requestAnimationFrame(this.update.bind(this));
         this.render();
-        this.cube.rotation.x += 0.05;
     }
 
 
