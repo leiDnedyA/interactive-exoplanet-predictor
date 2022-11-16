d69501e6744fc2485298f9fca5b13ffa961deb6c
Ayden Diel <aydendiel@gmail.com>
2022-11-16 13:37:18 -0500

    updated model path to work on unix and win32

--- Backend/Tree_algorithm/model_predictor.py
+++ Backend/Tree_algorithm/model_predictor.py
@@ -1,6 +1,7 @@
 import joblib
+from sys import platform
 
 def Predict_Simple(x):
     # Load the model
-    planet_predictor = joblib.load('Backend\Tree_algorithm\Saved_models\exoplanet_predictor.joblib')
+    planet_predictor = joblib.load(('Backend\Tree_algorithm\Saved_models\exoplanet_predictor.joblib' if platform == "win32" else 'Backend/Tree_algorithm/Saved_models/exoplanet_predictor.joblib'))
     return planet_predictor.predict(x)
