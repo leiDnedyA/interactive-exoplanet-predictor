319bcbc698fd050eeeac2fc2753567436d876d9f
leiDnedyA <aydendiel@gmail.com>
2023-11-02 09:18:20 -0400

    renamed scripts to exclude .sh extension, added 'clean' script to remove last build

--- buildstart.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-bash build.sh
-
-bash api.sh & xdg-open http://localhost:3001/
-
-exit 0
--- devstart.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-bash api.sh -d & bash client.sh && fg
-
-exit 0
