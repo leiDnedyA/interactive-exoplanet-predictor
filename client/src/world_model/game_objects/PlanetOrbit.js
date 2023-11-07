5cc41736a734b1fbcaaf71f3b7a007fdb7f4f282
leiDnedyA <aydendiel@gmail.com>
2023-11-07 09:08:34 -0500

    Added planet orbiting

--- client/src/world_model/game_objects/PlanetOrbit.js
+++ client/src/world_model/game_objects/PlanetOrbit.js
@@ -5,13 +5,34 @@ import MeshGameObject from './MeshGameObject';
 const CIRCLE_THICKNESS = .05;
 
 class PlanetOrbit extends MeshGameObject{
-    constructor(radius = 1, orbitDistance = 1, color = 0x0000ff){
-	super(new THREE.Vector3(0, 0, 0),
-		new THREE.RingGeometry(orbitDistance, orbitDistance + CIRCLE_THICKNESS, 100),
-		new THREE.MeshBasicMaterial({color: 0xffffff, side: THREE.DoubleSide}));
-	this.planet = new Planet(new THREE.Vector3(orbitDistance, 0, 0), radius, color);
-	this.mesh.attach(this.planet.mesh);
-    }
+	constructor(radius = 1, orbitDistance = 1, color = 0x0000ff){
+		super(new THREE.Vector3(0, 0, 0),
+			new THREE.RingGeometry(orbitDistance, orbitDistance + CIRCLE_THICKNESS, 100),
+			new THREE.MeshBasicMaterial({color: 0xffffff, side: THREE.DoubleSide}));
+		this.orbitDistance = orbitDistance;
+		this.orbitSpeed = 1;
+
+		this.planet = new Planet(new THREE.Vector3(orbitDistance, 0, 0), radius, color);
+
+		this.orbitState = 0; // number 0 - 2*pi for where the planet is in its orbit
+
+		this.mesh.attach(this.planet.mesh);
+		this.started = true;
+	}
+	update(deltaTime){
+		if (this.started !== true){
+			return;
+		}
+
+		console.assert(this.planet !== undefined);
+		this.orbitState += this.orbitSpeed * (deltaTime / 1000);
+		
+		if (this.orbitState >= 2 * Math.PI){
+			this.orbitState = this.orbitState - (2 * Math.PI);
+		}
+
+		this.planet.mesh.position.set(Math.cos(this.orbitState) * this.orbitDistance, Math.sin(this.orbitState) * this.orbitDistance, 0);
+	}
 }
 
 export default PlanetOrbit;
